437fdce54df174092b399f671effd84e060aaf76
Justin Beckwith <justin.beckwith@gmail.com>
2011-11-17 02:48:20 -0800

    made mobile work

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,18 +1,28 @@
 $(function() {
+
+  var name = prompt('what is your name?') || 'WebMatrix User';
+
   var socket = io.connect();
   
-  socket.on('connect', function() {
-    alert('connect');
+  socket.on('connect', function () {
+    socket.emit('setname', name);
+    $("#chat").append($("<div class=\"system\">you have joined the party</div>"));
   });
 
-  socket.on('disconnect', function() {
-    alert('disconnect');
+
+  socket.on('announcement', function(data) {
+    $("#chat").append($("<div class=\"system\">" + data.announcement + "</div>"));
   });
 
   socket.on('message', function (data) {
-    //$("#chat").append($("<div>" + data.mesage + "</div>"));
-    alert('message')
+    $("#chat").append($("<div><span class=\"user\">" + data.message[0] + ":&nbsp;</span>" + data.message[1] + "</div>"));
   });	
+
+  socket.on('messages', function (data) {
+  	for (var i=0; i<data.buffer.length; i++) {
+    	$("#chat").append($("<div><span class=\"user\">" + data.buffer[i].message[0] + ":&nbsp;</span>" + data.buffer[i].message[1] + "</div>"));
+    } 
+  });
 
   $("#send").click(function(e){
   	e.preventDefault();
@@ -21,4 +31,5 @@ $(function() {
   	$("#chat").append($("<div><span class=\"user-me\">me:&nbsp;</span>" + msg + "</div>"));
   	$("#message").val('');
   });
+
 })
